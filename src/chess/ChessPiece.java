79daa716c4e7905d7f0241b7dd3a82adcb364d97
LuizReis-dev <reisluiz05@gmail.com>
2021-07-20 13:06:46 -0300

    Implementando logica de check e corringindo bugs

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,28 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
+
+public abstract class ChessPiece extends Piece {
+
+	private Color color;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
+}
